668f957d90d734ca0db3a77f0996f11aa9604fb9
mw2020r-creator <mw2020r@gmail.com>
2025-11-22 17:13:53 +0100

    Add preemptible instances to worker configuration

--- modules/dataproc/main.tf
+++ modules/dataproc/main.tf
@@ -127,12 +127,12 @@ resource "google_dataproc_cluster" "tbd-dataproc-cluster" {
 
     worker_config {
       num_instances = 2
+      num_preemptible_instances  = 2
       machine_type  = var.machine_type
       disk_config {
         boot_disk_type    = "pd-standard"
         boot_disk_size_gb = 100
       }
-
     }
   }
 }
